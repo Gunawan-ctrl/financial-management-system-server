000deb4e9c24d49066ff6d9b203720924c8bbf7f
gunawan <gnnwan.dev@gmail.com>
2026-07-10 22:17:37 +0700

    refactor: simplify database connection handling in index.ts

--- index.ts
+++ index.ts
@@ -1,32 +1,11 @@
-// import mongoose from "mongoose";
-// import dbconfig from "./src/config/dbconfig.ts";
-// import app from "./src/app.ts";
-
-// mongoose.set("strictQuery", false);
-// mongoose
-//   .connect(dbconfig.MONGO_URL)
-//   .then(() => console.log("Berhasil terhubung ke database"))
-//   .catch((err) => console.error("Gagal terhubung ke database", err));
-
-// app.listen(dbconfig.PORT, () => console.log(`Server berjalan di port ${dbconfig.PORT}`));
-
 import mongoose from "mongoose";
 import dbconfig from "./src/config/dbconfig.ts";
 import app from "./src/app.ts";
 
-let dbConnected = false;
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(dbconfig.MONGO_URL)
+  .then(() => console.log("Berhasil terhubung ke database"))
+  .catch((err) => console.error("Gagal terhubung ke database", err));
 
-export default async function handler(req: any, res: any) {
-  try {
-    if (!dbConnected) {
-      mongoose.set("strictQuery", false);
-      await mongoose.connect(dbconfig.MONGO_URL);
-      dbConnected = true;
-    }
-
-    return app(req, res);
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ message: "Internal server error" });
-  }
-}
+app.listen(dbconfig.PORT, () => console.log(`Server berjalan di port ${dbconfig.PORT}`));
